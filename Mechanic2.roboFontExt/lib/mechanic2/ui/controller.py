3ddc76e8279b7b46ffee7ea14382c93e2f5bea64
Frederik Berlaen <frederik@typemytype.com>
2022-05-30 15:16:44 +0200

    Dont update while reading extension, only when everything is loaded

--- Mechanic2.roboFontExt/lib/mechanic2/ui/controller.py
+++ Mechanic2.roboFontExt/lib/mechanic2/ui/controller.py
@@ -149,7 +149,7 @@ class MechanicController(BaseWindowController):
     def _makeExtensionItem(self, extensionData, itemClass, url):
         try:
             item = MCExtensionListItem(
-                itemClass(extensionData, checkForUpdates=self._shouldCheckForUpdates)
+                itemClass(extensionData)
             )
             self._wrappedItems.append(item)
         except Exception as e:
@@ -197,7 +197,7 @@ class MechanicController(BaseWindowController):
         for singleExtension in getExtensionDefault("com.mechanic.singleExtensionItems"):
             try:
                 item = MCExtensionListItem(
-                    ExtensionYamlItem(singleExtension, checkForUpdates=self._shouldCheckForUpdates)
+                    ExtensionYamlItem(singleExtension)
                 )
                 self._wrappedItems.append(item)
             except Exception as e:
